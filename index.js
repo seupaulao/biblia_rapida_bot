125f61df1f405360a250b3f4de347e0ef50769a0
81784244368 <paulo.silva-junior@serpro.gov.br>
2025-07-22 11:00:05 -0300

    função ler verso

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
+const util = require("./util");
 const { Telegraf, Markup, session } = require("telegraf")
-const { message } = require("telegraf/filters")
+//const { message } = require("telegraf/filters")
+
+require('dotenv').config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
+bot.use(session());
+
+function listarLivros() {
+   let vetor = [];
+   const c = util.get_livros();
+   for (let i = 0; i < c.length; i++) {
+       let v = [];
+       v.push(Markup.button.callback(util.get_livro(i), util.get_ref(i))); 
+       vetor.push(v);
+   }
+   return Markup.inlineKeyboard(vetor);
+}
+
 function comandos(ctx, mensagem) { 
+   ctx.session = null;
    ctx.reply(mensagem, Markup.inlineKeyboard([
-      [Markup.button.callback('Ler um capítulo', 'lercapitulo')],
-      [Markup.button.callback('Ler um verso','lrverso')],
-      [Markup.button.callback('Ler um verso por referencia','irreferencia')],
-      [Markup.button.callback('Ver referencias dos livros','referencias')],
-      [Markup.button.callback('Pesquisar uma palavra','pesquisar')],
-      [Markup.button.callback('Pesquisar no dicionário','dicionario')],
-      [Markup.button.callback('Olhar o catálogo do dicionário','catalogo')],
+      [Markup.button.callback('Ler um capítulo', 'oplercapitulo')],
+      [Markup.button.callback('Ler um verso','oplerverso')],
+      [Markup.button.callback('Ler um verso por referencia','opirreferencia')],
+      [Markup.button.callback('Ver referencias dos livros','opreferencias')],
+      [Markup.button.callback('Pesquisar uma palavra','oppesquisar')],
+      [Markup.button.callback('Pesquisar no dicionário','opdicionario')],
+      [Markup.button.callback('Olhar o catálogo do dicionário','opcatalogo')],
    ]));
 }
 
@@ -19,6 +36,16 @@ bot.start((ctx)=>comandos(ctx, 'Bem-vindo ao Bíblia Rápida!'))
 bot.hears('Ajuda', (ctx)=>comandos(ctx, 'Os comandos são:'))
 bot.hears('Me ajude', (ctx)=>comandos(ctx, 'Os comandos são:'))
 
-bot.action("ler verso", (ctx)=>{
-   ctx.reply("Escolha o Livro;", listarLivros(ctx))
+bot.action("oplerverso", (ctx)=>{
+   ctx.reply("Escolha o Livro:", listarLivros())
 })
+
+for (let i=0; i < util.get_livros().length; i++)
+{
+   bot.action(util.get_ref(i), (ctx) => {
+      ctx.reply(`O livro selecionado foi ${util.get_livro(i)}`);
+      ctx.session.livro=util.get_ref(i);
+      
+   })
+}
+bot.launch();
--- util.js
+++ util.js
@@ -1,14 +1,20 @@
-const livros_biblia_refs= [
-    "GEN","EXO","LEV","NUM","DEU","JOS","JDG","RUT","1SA","2SA",
-    "1KI","2KI","1CH","2CH","EZR","NEH","EST","JOB","PSA","PRO",
-    "ECC","SOL","ISA","JER","LAM","EZE","DAN","HOS","JOE","AMO",
-    "OBA","JON","MIC","NAH","HAB","ZEP","HAG","ZEC","MAL","MAT",
-    "MAR","LUK","JOH","ACT","ROM","1CO","2CO","GAL","EPH","PHI",
-    "COL","1TH","2TH","1TI","2TI","TIT","PHM","HEB","JAM","1PE",
-    "2PE","1JO","2JO","3JO","JUD","REV"
-];
+const qtdversos = require('./qtdversos');
 
-const livros_biblia = [
+exports.get_refs = () => {
+    return [
+        "GEN","EXO","LEV","NUM","DEU","JOS","JDG","RUT","1SA","2SA",
+        "1KI","2KI","1CH","2CH","EZR","NEH","EST","JOB","PSA","PRO",
+        "ECC","SOL","ISA","JER","LAM","EZE","DAN","HOS","JOE","AMO",
+        "OBA","JON","MIC","NAH","HAB","ZEP","HAG","ZEC","MAL","MAT",
+        "MAR","LUK","JOH","ACT","ROM","1CO","2CO","GAL","EPH","PHI",
+        "COL","1TH","2TH","1TI","2TI","TIT","PHM","HEB","JAM","1PE",
+        "2PE","1JO","2JO","3JO","JUD","REV"
+    ];
+    
+}
+
+exports.get_livros = () => { 
+    return [
     "GÊNESIS","ÊXODO","LEVÍTICO","NÚMEROS","DEUTERONÔMIO",
     "JOSUÉ","JUÍZES","RUTE","I SAMUEL","II SAMUEL",
     "I REIS","II REIS","I CRÔNICAS","II CRÔNICAS","ESDRAS",
@@ -22,5 +28,39 @@ const livros_biblia = [
     "HEBREUS","TIAGO","I PEDRO","II PEDRO","I JOÃO","II JOÃO",
     "III JOÃO","JUDAS","APOCALIPSE"
 ];
+}
 
+exports.get_livro = (i) => { 
+    const livros = this.get_livros();
+    return livros[i];
+}
+
+exports.get_ref = (i) => { 
+    const refs = this.get_refs();
+    return refs[i];
+}
+
+exports.get_capitulos = (i) => { 
+    const capitulosPorLivro = [
+    50, 40, 27, 36, 34,  // Pentateuco
+    24, 21, 4, 31, 24,   // Josué – 2 Samuel
+    22, 25, 29, 36, 10,  // 1 Reis – Esdras
+    13, 10, 42, 150, 31, // Neemias – Provérbios
+    12, 8, 66, 52, 5,    // Eclesiastes – Lamentações
+    48, 12, 14, 3, 9,    // Ezequiel – Amós
+    1, 4, 7, 3, 3,       // Obadias – Sofonias
+    2, 14, 4,            // Ageu – Malaquias
+    28, 16, 24, 21, 28,  // Evangelhos e Atos
+    16, 16, 13, 6, 6,    // Romanos – Efésios
+    4, 4, 5, 3, 6,       // Filipenses – 1 Timóteo
+    4, 3, 1, 13, 5,      // 2 Timóteo – Tiago
+    5, 3, 5, 1, 1,       // 1 Pedro – 3 João
+    1, 22               // Judas – Apocalipse
+  ];
+   return capitulosPorLivro[i];
+}
+
+exports.get_qtd_versos = (posicaoLivro, capitulo) => {
+    return qtdversos.get_qtd_versos(posicaoLivro, capitulo);
+}
 
